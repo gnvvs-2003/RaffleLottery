b2aad5d9c82286db576cd8b39152a1dd061f85eb
gnvvs-2003 <vchess007@gmail.com>
2025-07-02 00:49:49 +0530

    Added chain link and request id

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,21 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.4.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+import {VRFV2PlusClient} from "@chainlink/contracts@1.4.0/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title Raffle smart contract
  * @author gnvvs-2003
  * @notice Implementation of chainlink VRF2_5
  */
-
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     // requirements to enter raffle (entrance fee,players,times,interval)
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    bytes32 private immutable i_keyHash;
 
     address[] private s_players;
     uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    uint16 private constant REQUEST_CONFIRMATIONS = 4;
+    uint32 private constant NUM_WORDS = 1;
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         // entrance fee
         i_entranceFee = entranceFee;
         // duration of lottery
@@ -37,7 +50,7 @@ contract Raffle {
         s_players.push(payable(msg.sender));
     }
 
-    function pickWinner() external view {
+    function pickWinner() external {
         /**
          * @note : Steps to pick winner after the interval of the raffle is completed
          * @note : pick a random number -> use random number to call the winner -> automate the pick winner function
@@ -46,12 +59,38 @@ contract Raffle {
             // interval yet to complete
             revert Raffle__notEnoughTimePassed();
         }
+
         // get random number
         // call the winner using random number
         // automate the program
+
+        /**
+         * @dev : Adding the random number using chainlink contracts
+         */
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                /**
+                 * @dev : keyHash,subscriptionId,requestConfirmations,callbackGasLimit,numWords,nativePayment declared in top
+                 */
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                ) // new parameter
+            });
+        uint256 requestID = s_vrfCoordinator.requestRandomWords(request);
     }
 
     // custom errors
     error Raffle__sendMoneyToEnterRaffle();
     error Raffle__notEnoughTimePassed();
+
+    // interface functions
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {}
 }
