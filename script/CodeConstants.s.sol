bf68590fa4fd5cf33acbbff6baf282d31f8f22bc
gnvvs-2003 <vchess007@gmail.com>
2025-07-03 16:59:08 +0530

    added sepolia network config to HelperConfig.sol

---
+++ script/CodeConstants.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+abstract contract CodeConstants {
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+}
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {CodeConstants} from "./CodeConstants.s.sol";
 
-contract HelperConfig is Script {
+contract HelperConfig is Script, CodeConstants {
     struct NetworkConfig {
         uint256 entranceFee;
         uint256 interval;
@@ -19,10 +20,23 @@ contract HelperConfig is Script {
 
     /**
      * @dev : getting config by chainId
-    function getConfigByChainId(uint256 chainId) public returns(NetworkConfig memory){
-
-    }
      */
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
+        // if network config has chain id configs
+        if (networkConfig[chainId].vrfCoordinator != address(0)) {
+            return networkConfig[chainId];
+        }
+        // for local chain
+        else if (networkConfig[chainId] == LOCAL_CHAIN_ID) {
+            // return getOrCreateAnvilEthConfig();
+        }
+        // for sepolia config
+        else {
+            // return getSepoliaEthConfig();
+        }
+    }
 
     /**
      * @dev : local network
@@ -33,10 +47,17 @@ contract HelperConfig is Script {
 
     /**
      * @dev : sepolia network
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory){
-        return NetworkConfig(
-            
-        );
     }
      */
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30, // 30 seconds
+                vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+                gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                callbackGasLimit: 500000,
+                subscriptionId: 0 // for temporary
+            });
+    }
 }
